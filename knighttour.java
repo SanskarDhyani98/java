4aab24b6baee3d915cca098be0531e68dba67d43
Sanskar_Dhyani <sanskardhyani98@gmail.com>
2023-07-17 10:53:04 +0530

    corrections

--- knighttour.java
+++ knighttour.java
@@ -19,6 +19,47 @@ public class knighttour {
             System.out.println();
         }
     }
+    public static boolean solveKTutil(int x,int y,int sol[][],int c,int[]xs,int[]ys)
+    {
+        if(c==N*N)
+        {  
+            // printSolution(sol);
+            return true;
+        }
+    
+    for(int i=0;i<8;i++)
+    {  int next_x=x+xs[i];
+        int next_y=y+ys[i];
+        if(isSafe(next_x, next_y, sol))
+        {  sol[next_x][next_y]=c;
+            if(solveKTutil(next_x,next_y,sol,c+1,xs,ys))
+            return true;
+            else
+            sol[next_x][next_y]=-1;
+        }
+    }
+    return false;
+    }
+    public static void solveKT()
+    {{
+    int xs[]={1,2,-1,-1,1,2,-2,-2};;
+    int ys[]={2,1,2,-2,-2,-1,1,-1};
+    int sol[][]=new int[N][N];
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        sol[i][j]=-1;
+    }
+    sol[0][0]=0;
+      if(!solveKTutil(0,0,sol,1,xs,ys))
+      {
+         System.out.println("Solution does not exist");
+      }
+      else{
+        System.out.println("Solution exists");
+        printSolution(sol);
+      }
+    }}
     public static void main(String args[])
     {
         // Function Call
